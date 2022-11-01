d1861c78bc39f745c603686938184fc98a47998f
namju0815 <namju990815@naver.com>
2022-11-01 23:58:12 +0900

    finally6

---
+++ HW3/UBERStudent20201016.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+import sys
+from datetime import datetime, date
+inputfile = input()
+outfile = input()
+f = open(inputfile,'r')
+def day_week(date):
+	week=['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+	number = date.weekday()
+	return week[number]
+	
+w = open(outfile,'w')
+line = f.readline()
+while line:
+	fline = line.split(",")
+	fline = list(map(lambda s: s.strip(), fline))
+	wline = fline[1].split("/")
+	wline = list(map(lambda s: s.strip(), wline))
+	day = day_week(date(int(wline[2]), int(wline[0]), int(wline[1])))
+	data = ('{},{} {},{}'.format(fline[0], day, fline[2], fline[3]))
+	w.write(data)
+	w.write("\n")
+	line = f.readline()
+	
+w.close()
+f.close()
