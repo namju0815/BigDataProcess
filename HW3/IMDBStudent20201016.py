d8edb73758564a5031f3c8c57d9a18f1f59b7466
namju0815 <namju990815@naver.com>
2022-11-01 23:10:31 +0900

    finally2

--- HW3/IMDBStudent20201016.py
+++ HW3/IMDBStudent20201016.py
@@ -2,8 +2,23 @@
 import sys
 f = open('movies.dat','r')
 line = f.readline()
+genre={'Animation' : 0, "Children's" : 0, 'Comedy' : 0, 'Adventure' : 0, 'Fantasy' : 0, 'Romance' : 0, 'Drama' : 0, 'Action' : 0, 'Crime' : 0, 'Thriller' : 0, 'Horror' : 0, 'Sci-Fi' : 0, 'Documentary' : 0}
+keylist = list(genre.keys())
+
 while line:
-	a = line.split("::")
-	print("ID : {}, Title : {}, Genre : {}".format(a[0], a[1], a[2]), end ="")
+	fline = line.split("::")
+	gline = fline[2].split("|")
+	gline = list(map(lambda s: s.strip(), gline))
+	for i in gline:
+		for k in keylist:
+			if(i == k):
+				genre[k]+=1
 	line = f.readline()
+
+valuelist = list(genre.values())
+index = 0
+while valuelist:
+	print('{} {}'.format(keylist[index], valuelist[index]))
+	index+=1
+	
 f.close()
