fa3231409eee0d11e6d9393b1cd544e3735452e9
namju0815 <namju990815@naver.com>
2022-11-02 04:13:39 +0900

    finally14

--- HW3/UBERStudent20201016.py
+++ HW3/UBERStudent20201016.py
@@ -6,22 +6,34 @@ outputfile = sys.argv[2]
 
 f = open(inputfile,'r')
 def day_week(date):
-	week=['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
+	weekday=['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 	number = date.weekday()
-	return week[number]
+	return weekday[number]
 	
 w = open(outputfile,'w')
 line = f.readline()
+
+number = dict()
+llist=[]
 while line:
 	fline = line.split(",")
 	fline = list(map(lambda s: s.strip(), fline))
+	i = fline[0]
+	if i not in number:
+		number[i]=1
+	else:
+		number[i]+=1
+		
 	wline = fline[1].split("/")
 	wline = list(map(lambda s: s.strip(), wline))
 	day = day_week(date(int(wline[2]), int(wline[0]), int(wline[1])))
-	data = ('{},{} {},{}'.format(fline[0], day, fline[2], fline[3]))
-	w.write(data)
-	w.write("\n")
+	llist.append([fline[0], day, fline[2], fline[3]])
 	line = f.readline()
-	
+
+llist.sort()
+for i in llist:
+	data = ('{},{} {},{}'.format(i[0],i[1],i[2],i[3]))
+	w.write(data)
+	w.write('\n')	
 w.close()
 f.close()
